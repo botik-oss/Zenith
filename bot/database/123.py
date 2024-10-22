4a82880d523c0d688c552671c05546f8cbdaf85a
John Titor <kuzmin-grishenka@mail.ru>
2024-10-22 12:20:18 +0700

    gfadg

---
+++ bot/database/123.py
@@ -0,0 +1,9 @@
+import asyncio
+
+from admins_database import admins
+from clients_database import clients
+from clients_telegram_database import clients_telegram
+
+async def fadsf():
+    print(await clients.get_clients_number_with_birthday())
+asyncio.run(fadsf())
