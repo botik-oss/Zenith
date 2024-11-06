c81fab87b5228416c3d0af80a5d3224878818bd0
Wingelshteyn <wingelshteyn@gmail.com>
2024-11-06 15:09:08 +0700

    финал

--- bot/core/config.py
+++ bot/core/config.py
@@ -2,6 +2,6 @@ CLIENTS_DATABASE = "database/clients.db"  # Путь к бд клиентов
 
 ADMINS_DATABASE = 'database/admins.db'  # Путь к бд админов
 
-TOKEN = '7536990395:AAFpT5VXsx0VuBuqoG5ha7h5pzeBQCIG1SM'  # Токен бота
+TOKEN = '7859510119:AAEGBXuw1f52n14AI6MDX6vUFG14ol580Vc'  # Токен бота
 
 MAIN_ADMIN_ID = "1795780447"
